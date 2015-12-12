50c0dc655d72531f603f40bdad13fc56323a355a
Boris Kudryavtsev <boriskk.work@gmail.com>
2015-12-11 23:41:21 -0800

    Generate random direction on correct keystroke

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
-var directions = ['up', 'down', 'left', 'right'];
+var directions = ["up", "down", "left", "right"];
+var keycodes = {"up": 38, "down": 40, "left": 37, "right": 39};
+var direction = directions[Math.floor(Math.random() * directions.length)];
 
 // set initial direction
-$('#direction').html('<h1>' + directions[Math.floor(Math.random() * directions.length)] + '</h1>');
+$("#direction").html("<h1>" + direction + "</h1>");
+
+$(function () {
+  $(document).keyup(function (e) {
+    if(e.keyCode == keycodes[direction]) {
+      direction = directions[Math.floor(Math.random() * directions.length)];
+      $("#direction").html("<h1>" + direction + "</h1>");
+    }
+  });
+});
