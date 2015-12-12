3fe415c5d162a2b4a6d7c7ede634fd908bffacef
Boris Kudryavtsev <boriskk.work@gmail.com>
2015-12-11 22:33:51 -0800

    Use jQuery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
 var directions = ['up', 'down', 'left', 'right'];
 
 // set initial direction
-document.getElementById('direction').innerHTML = '<h1>' + directions[Math.floor(Math.random() * directions.length)] + '</h1>';
+$('#direction').html('<h1>' + directions[Math.floor(Math.random() * directions.length)] + '</h1>');
