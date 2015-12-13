d34ed0077d0d160dde2ab26438e944ce08b3df84
Boris Kudryavtsev <boriskk.work@gmail.com>
2015-12-12 18:36:35 -0800

    Add score display and lower time limit

--- js/main.js
+++ js/main.js
@@ -2,12 +2,17 @@ var directions = ["up", "down", "left", "right"];
 var keycodes = {"up": 38, "down": 40, "left": 37, "right": 39};
 var direction = directions[Math.floor(Math.random() * directions.length)];
 
+var game_on = true;
+var score = 0;
+
 // set initial direction
 $("#direction").html("<h1>" + direction + "</h1>");
 
 $(function () {
   $(document).keyup(function (e) {
-    if(e.keyCode == keycodes[direction]) {
+    if(game_on && e.keyCode == keycodes[direction]) {
+      score++;
+
       clearInterval(counter);
       counter = setInterval(timer, 10); // 10 will run the timer every 100th of a second
 
@@ -18,13 +23,22 @@ $(function () {
 });
 
 // countdown timer ------------------------
-var initial = 6400; // ms
+var initial = 1600; // ms
 var count = initial;
 var counter;
 
 function timer() {
     if (count <= 0) {
         clearInterval(counter);
+
+        game_on = false;
+
+        $("#direction").fadeOut(500);
+        $("#countdown").fadeOut(500);
+
+        $("#score").html("<h1>score: " + score + "</h1>");
+        $("#score").fadeIn(500);
+
         return;
     }
     count--;
